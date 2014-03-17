5e584b0a1511bd8184d59dd0d0fa4c1cf3bd6f71
Dmitry Cheryasov <dcheryasov@google.com>
2014-03-17 12:22:57 -0400

    Added 'color none'; listed all standard colors.

--- shell/colors.sh
+++ shell/colors.sh
@@ -6,14 +6,24 @@ declare -A color_by_name
 color_by_name[black]=0
 color_by_name[red]=1
 color_by_name[green]=2
+color_by_name[brown]=3
 color_by_name[blue]=4
 color_by_name[purple]=5
 color_by_name[cyan]=6
-color_by_name[white]=7
+color_by_name[gray]=7
+color_by_name[dk_gray]=8
+color_by_name[lt_red]=9
+color_by_name[lt_green]=10
+color_by_name[yellow]=11
+color_by_name[lt_blue]=12
+color_by_name[lt_purple]=13
+color_by_name[lt_cyan]=14
+color_by_name[white]=15
 # TODO more
 
 color() {
-# possible invocations
+# possible invocation arguments
+# "none"  // resets all attributes
 # "yellow"
 # "yellow on blue"
 # "on blue"
@@ -21,6 +31,10 @@ color() {
     local is_bg='' # empty is false
     local result=''
     while [ -n "$1" ]; do
+        if [ $1 = 'none' ]; then
+            result="$(tput sgr none)"
+            break
+        fi
         if [ $1 = 'on' ]; then
             shift
             is_bg=1
@@ -32,5 +46,6 @@ color() {
             shift
         fi
     done
-    echo $result
+    echo -n $result
 }
+
