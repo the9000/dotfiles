02663201bbecbda77529cafe4ee8fe23c77a50fa
Dmitry Cheryasov <dcheryasov@google.com>
2015-01-20 15:55:49 -0500

    Color of selected line in Emacs.

--- emacs/.emacs.d/init.d/80-colors.el
+++ emacs/.emacs.d/init.d/80-colors.el
@@ -9,7 +9,7 @@
 ; TODO: consider a fully custom theme
 
 (global-hl-line-mode t) ; highlight current line for all modes by default
-(set-face-background 'hl-line "gray24") ;; current line
+(set-face-background 'hl-line "gray16") ;; current line
 (set-face-background 'fringe "gray16")
 
 ;; The rest of coloring is done via customize and lives in .emacs
