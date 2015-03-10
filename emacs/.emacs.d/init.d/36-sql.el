23c6e56dedc2a58de42cee225a7b1d79ab2c7f65
Dmitry Cheryasov <dcheryasov@google.com>
2015-03-10 15:55:48 -0400

    Initial customizations to SQL mode.

--- emacs/.emacs.d/init.d/36-sql.el
+++ emacs/.emacs.d/init.d/36-sql.el
@@ -1,2 +1,3 @@
 ;; Do not wrap lines in SQL console mode.
-(add-hook 'sql-interactive-mode-hook '(lambda () (toggle-truncate-lines 1))
+(add-hook 'sql-interactive-mode-hook '(lambda () (toggle-truncate-lines 1)))
+
