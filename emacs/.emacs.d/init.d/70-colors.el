460181b49f0ed0a782d3d0177a0d4aef75296662
Dmitry Cheryasov <dcheryasov@handshake.com>
2016-12-12 15:51:18 -0500

    Commented out the direct theme invocation in 70-colors.
    Install color-theme-modern; .emacs info suffices.

--- emacs/.emacs.d/init.d/70-colors.el
+++ emacs/.emacs.d/init.d/70-colors.el
@@ -5,11 +5,11 @@
 ;  nil
 ;)
 (require 'color-theme)
-(load-theme 'gray30)
+;; This is not required any more: (load-theme 'gray30)
 ; TODO: consider a fully custom theme
 
 (global-hl-line-mode t) ; highlight current line for all modes by default
-(set-face-background 'hl-line "gray16") ;; current line
+(set-face-background 'hl-line "black") ;; current line
 (set-face-background 'fringe "gray16")
 
 ;; The rest of coloring is done via customize and lives in .emacs
