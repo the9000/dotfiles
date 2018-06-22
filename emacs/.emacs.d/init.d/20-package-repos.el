723deaa45f61daabe05f8616210d153d1c8e5c85
Dmitry Cheryasov <dmitry.cheryasov@gmail.com>
2018-06-22 18:47:01 -0400

    Added 20-package-repos with marmalade and melpa.

---
+++ emacs/.emacs.d/init.d/20-package-repos.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/vendors/" t)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
