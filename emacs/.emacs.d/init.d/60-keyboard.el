ab5fee3f7377b1f930802eaa008fef1423502ff3
Dmitry Cheryasov <dmitry.cheryasov@gmail.com>
2018-06-22 18:43:09 -0400

    Keys: magit, outline-mode, del selection, desktop save.

--- emacs/.emacs.d/init.d/60-keyboard.el
+++ emacs/.emacs.d/init.d/60-keyboard.el
@@ -26,3 +26,49 @@
 
 ; M-click opens file at mouse
 (global-set-key (kbd "<M-down-mouse-1>") 'ffap-at-mouse)
+
+;; Package-specifc niceties.
+;; // TODO: move to a separate file, like minor-modes.el.
+;; Make major mode setup files consult or inform it.
+
+(delete-selection-mode 1)
+(if (boundp 'back-button) (back-button-mode t))
+
+(desktop-save-mode t)
+
+(if (boundp 'fixmee-mode) (fixmee-mode t))
+(if (boundp 'recent-mode) (recent-mode t))
+
+(if (fboundp 'find-file-in-repository)
+    (global-set-key (kbd "M-g r") 'find-file-in-repository))
+
+(if (fboundp 'string-inflection-all-cycle)
+    (global-set-key (kbd "C-c -") 'string-inflection-all-cycle))
+
+(if (fboundp 'magit-status)
+    (global-set-key (kbd "C-x g") 'magit-status))
+
+(if (fboundp 'ace-window)
+    (global-set-key (kbd "C-S-a") 'ace-window))
+
+;; \\
+
+; Outline mode keys rebound more nicely
+(defun my-outline-mode-key-bindings ()
+  "Usable whenever outline (minor) mode is invoked."
+  (interactive)
+  (local-set-key (kbd "C-c <up>") 'hide-subtree)
+  (local-set-key (kbd "C-c <down>") 'show-subtree)
+  (local-set-key (kbd "C-S-c <up>") 'hide-body)
+  (local-set-key (kbd "C-S-c <down>") 'show-all)
+)
+
+(add-hook 'outline-mode-hook 'my-outline-mode-key-bindings)
+(add-hook 'outline-minor-mode-hook 'my-outline-mode-key-bindings)
+
+; Delete / yank overwrites regions
+(delete-selection-mode 1)
+
+
+; Always ask before exiting.
+(setq confirm-kill-emacs 'y-or-n-p)
