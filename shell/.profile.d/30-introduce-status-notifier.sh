d63aa7cf89bd63cfaa6f9173ebffc78ea78e81fe
Dmitry Cheryasov <dcheryasov@grubhub.com>
2019-05-07 13:28:53 -0400

    Shell init: timezone, locale, N and NN, run setup prompt.

---
+++ shell/.profile.d/30-introduce-status-notifier.sh
@@ -0,0 +1,39 @@
+# Notifies of execution of something.
+# $* is the command to execute.
+# If no command given, the last ststus is used, and command name is not known..
+
+# First thing we want is to capture the last status of a possible pipeline.
+function N() {
+    # Should work even with embedded whitespace.
+    local start_seconds=$(date +"%s")
+    "$@"
+    status=$?
+    command="$*"
+    local duration="in $(($(date +'%s') -  ${start_seconds})) s"
+    _notify_by_status "${status}" "${duration}" "${command}"
+    (exit ${status})  # Exit in a subshell sets exit code but does not exit our shell.
+}
+
+# To be invoked after a long command, with optional $1 as name.
+function NN() {
+    local status=$?
+    local name="${*:-Previous command}"
+    _notify_by_status "${status}" "by now" "${name}"
+    (exit ${status})
+}
+
+function _notify_by_status() {
+    local status="$1"
+    local time_clause="$2"
+    shift 2
+    local command="$*"
+    if [ "${status}" -eq 0 ]; then
+        local message="Completed ${time_clause}."
+        local icon="up"
+    else
+        local message="Failed with code $status ${time_clause}."
+        local icon="error"
+    fi
+
+    notify-send -i "${icon}" "${command}" "${message}"
+}
