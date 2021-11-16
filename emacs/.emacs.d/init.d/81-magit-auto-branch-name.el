fb64b8c8a6968fd1109a0910a82806c42d9a2708
Dmitry Cheryasov <dmitry@dynaboard.com>
2021-11-16 09:03:01 +0000

    Emacs: handle prefix-slash in Magit branch names.
    Now branch foo-bar-123-quux pre-generates [BAR-123] in commit message.

--- emacs/.emacs.d/init.d/81-magit-auto-branch-name.el
+++ emacs/.emacs.d/init.d/81-magit-auto-branch-name.el
@@ -1,5 +1,5 @@
 (defun my-extract-branch-tag (branch-name)
-  (let ((TICKET-PATTERN "\\(?:[[:alpha:]]+-\\)?\\([[:alpha:]]+-[[:digit:]]+\\)-.*"))
+  (let ((TICKET-PATTERN "\\(?:[[:alpha:]]+-?/?\\)?\\([[:alpha:]]+-[[:digit:]]+\\)-.*"))
     (when (string-match-p TICKET-PATTERN branch-name)
        (s-upcase (replace-regexp-in-string TICKET-PATTERN "[\\1] " branch-name)))))
 
