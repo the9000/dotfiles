39ad46fd9a98f977a0f5f801afb0d47cfc8cf1ef
Dmitry Cheryasov <dmitry@dynaboard.com>
2022-02-21 16:00:52 -0500

    Pofile: an poor man's attempt to run xbanish once.
    Must really be started as a service.

--- shell/.profile.d/90-xbanish.sh
+++ shell/.profile.d/90-xbanish.sh
@@ -1,4 +1,8 @@
-if [ "${DISPLAY}" ] && [ -x $(which xbanish) ]; then
-  xbanish -i control -i mod3 -i mod4 &
+if [ "${DISPLAY}" ] && \
+       [ -x "$(which xbanish)" ] && \
+       [ -z "$(ps a | grep $(which xbanish) | grep -v grep)" ]
+then
+  # NOTE: Running with full path to make it greppable above.
+  $(which xbanish) -i control -i mod3 -i mod4 &
   disown %1
 fi
