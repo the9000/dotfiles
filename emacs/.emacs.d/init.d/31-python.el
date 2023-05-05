86d2c0118186e353d181c5844e55bd0b7e02f45d
Dmitry Cheryasov <dmitry@coffeemeetsbagel.com>
2023-05-05 07:31:31 -0400

    [Emacs] Python: venv, LSP, projectile, dumb-jump.
    Should expand to all prog modes (except LSP stuff).

--- emacs/.emacs.d/init.d/31-python.el
+++ emacs/.emacs.d/init.d/31-python.el
@@ -1,15 +1,30 @@
 ;; From http://cpbotha.net/thingies/python-outline.dot.emacs.txt
 ;; Makes code folding in Python mode work (allegedly).
 
+;;; Code:
+(require 'virtualenvwrapper)  ;; We just always want it.
+(if (require 'smart-jump nil t) ;; It's great to have, but semantic-mode works OK in simple cases.
+    (smart-jump-python-register)
+)
+
 (add-hook 'python-mode-hook 'my-python-hook)
 
 (defun py-outline-level ()
-  "This is so that `current-column` DTRT in otherwise-hidden text"
+  "This is so that `current-column` DTRT in otherwise-hidden text."
   ;; from ada-mode.el
   (let (buffer-invisibility-spec)
     (save-excursion
       (skip-chars-forward "\t ")
       (current-column))))
+
+(defun my-setup-py-lsp-mode ()
+  "Set up LSP-specific things for Python."
+  (if (boundp 'lsp-ui-sideline-ignore-duplicate) (setq lsp-ui-sideline-ignore-duplicate t))
+  (if (boundp 'lsp-mode)
+      (lsp-deferred)  ;; Language server.
+      (semantic-mode)  ;; Better than nothing without LSP, but does not play nice with it.
+    )
+)
 
 ; this fragment originally came from the web somewhere, but the outline-regexp
 ; was horribly broken and is broken in all instances of this code floating
@@ -22,20 +37,29 @@
   (outline-minor-mode t)
   ; initially hide all but the headers: (hide-body)
   (show-paren-mode 1)
-  ; always show trailing space
-  (setq show-trailing-whitespace 1)
   ; show nice indentation.
-  (if (fboundp 'highlight-indent-guides-mode) (highlight-indent-guides-mode))
-  ; show fixme warnings if the mode is available
-  (if (fboundp 'hl-todo-mode) (hl-todo-mode t))
-  ; Limit line length.
+  ;; (if (fboundp 'highlight-indent-guides-mode) (highlight-indent-guides-mode))
+  (my-enable-each-if-present
+   '(highlight-indent-guides-mode  ;; Indentation markers on the left.
+     hl-todo-mode ;; Hightlight TODO, etc.
+     highlight-symbol-mode  ;; Non-semantic highlight of symbol-like strings.
+     python-docstring-mode  ;; Highlight :param:-style syntax in docstrings.
+     flycheck-mode  ;; Better than nothing if no LSP.
+  ))
+  ;; Limit line length.
+  ;; TODO: use editroconfig.
   (if (fboundp 'column-enforce-n) (column-enforce-n 100))
-  ; Highlight symbol under cursor.
-  (if (fboundp 'highlight-symbol-mode) (highlight-symbol-mode))
-  ; Run Semantic mode.
-  (if (fboundp 'semantic-mode) (semantic-mode))
-  ; Flycheck.
-  (if (fboundp 'flycheck-mode) (flycheck-mode))
+  ; Always show trailing space
+  (setq show-trailing-whitespace 1)
   ;; kill trailing whitespace on save
   (add-to-list 'write-file-functions 'delete-trailing-whitespace)
+  (if (boundp 'projectile-mode)
+      (progn
+        (projectile-mode)
+        (venv-projectile-auto-workon) ;; Autoloaded from virtualenvwrapper.
+        ))
+  ;;
+  (if (fboundp 'dumb-jump-go-prompt)
+      (local-set-key (kbd "C-x .") 'dumb-jump-go-prompt))
+  (my-setup-py-lsp-mode)
 )
