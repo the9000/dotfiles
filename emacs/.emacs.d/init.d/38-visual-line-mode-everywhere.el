a244d2675ab081fab31006c5d17eca40d42e1c94
Dmitry Cheryasov (default) <dmitry.cheryasov@gmail.com>
2026-03-09 14:12:05 -0400

    [emacs] Visual line mode in text modes.

---
+++ emacs/.emacs.d/init.d/38-visual-line-mode-everywhere.el
@@ -0,0 +1,9 @@
+i;; Enable visual-line-mode everywhere words are involved.
+
+(defun my/visual-line-mode-enable () (visual-line-mode 1))
+
+(mapcar
+ (lambda (hook-alist)
+   (add-hook hook-alist #'my/visual-line-mode-enable))
+ '(text-mode-hook prog-mode-hook markdown-mode-hook org-mode-hook)
+ )
